1f725261954113a654dd7b2f8cee78a1149a3ba0
Golubian <gabrielchalifour@hotmail.com>
2024-02-05 15:16:27 -0500

    add isalnum, fix norm

---
+++ ft_isalnum.c
@@ -0,0 +1,22 @@
+static int	ft_isalpha(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+static int	ft_isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int ft_isalnum(char c)
+{
+    if (ft_isalpha(c) || ft_isdigit(c))
+        return (1);
+    return (0);
+}
