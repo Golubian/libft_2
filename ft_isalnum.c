2a2b86b07a89b39d219753468baaa845c0376c24
Gabriel Chalifour <gabrielchalifour@hotmail.com>
2024-02-06 15:01:05 -0500

    changes char to int in funcs

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,4 +1,4 @@
-static int	ft_isalpha(char c)
+static int	ft_isalpha(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
@@ -7,14 +7,14 @@ static int	ft_isalpha(char c)
 	return (0);
 }
 
-static int	ft_isdigit(char c)
+static int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
 
-int ft_isalnum(char c)
+int ft_isalnum(int c)
 {
     if (ft_isalpha(c) || ft_isdigit(c))
         return (1);
